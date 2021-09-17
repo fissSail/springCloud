a39ff5bb7158c908d5a5e11501fc8029886bcb9b
yff <1335799488@qq.com>
2021-09-17 22:02:07 +0800

    add jar

---
+++ cloud-config-center-3344/src/main/java/com/yff/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.yff.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author yanfeifan
+ * @Package com.yff.springcloud
+ * @Description
+ * @date 2021/9/17 21:35
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
